aceb52106c6b83f9ebb56d714e7f54d8d167d788
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2018-11-13 21:45:49 +0000

    Add Rect for detecting screen edges

---
+++ rect.js
@@ -0,0 +1,13 @@
+class Rect {
+  constructor(begin, end) {
+    this.begin = begin
+    this.end = end
+  }
+
+  intersectsCircle(circle) {
+    return circle.r.x - circle.radius > this.begin.x
+        && circle.r.x + circle.radius < this.end.x
+        && circle.r.y - circle.radius > this.begin.y
+        && circle.r.y + circle.radius < this.end.y
+  }
+}
