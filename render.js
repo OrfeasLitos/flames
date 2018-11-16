d476070042b836b95b5e1050fa1d7a99ecd65e14
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2018-11-16 16:25:19 +0000

    Spawn transparent, gradually make opaque

--- render.js
+++ render.js
@@ -1,8 +1,26 @@
+const BRIGHTEST = 0.7
+
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 const img = new Image()
 let imgLoaded = false
+
+function getOpacity(particle) {
+  const y = particle.r.y
+  const range = H - particle.radius
+  let opacity
+
+  if (y > range) {
+    opacity = 0
+  } else if (y > range*BRIGHTEST) {
+    const a = Math.PI/2/(range*(1-BRIGHTEST))
+    opacity = Math.sin(a*(range - y))
+  } else {
+    opacity = 0
+  }
+  return opacity
+}
 
 function renderParticle(particle) {
   const x = particle.r.x - particle.radius
@@ -14,6 +32,7 @@ function renderParticle(particle) {
   ctx.translate(particle.r.x, particle.r.y)
   ctx.rotate(particle.theta)
   ctx.translate(-particle.radius, -particle.radius)
+  ctx.globalAlpha = getOpacity(particle)
   ctx.drawImage(img, 0, 0, size, size)
   ctx.beginPath()
   ctx.arc(particle.r.x, particle.r.y, particle.radius, 0, 2*Math.PI)
